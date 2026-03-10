bf2baa57e6477882d77881762f0a754da221ca96
Marat <akhmetzyanovmm@gmail.com>
2026-03-10 16:32:53 +0300

    add Pydantic schemas for posts

--- app/api/posts.py
+++ app/api/posts.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 
+from app.schemas.post import PostResponse
+
 router = APIRouter(
     prefix="/posts",
     tags=["posts"],
 )
 
 
-@router.get("/{post_id}")
+@router.get("/{post_id}", response_model=PostResponse)
 async def get_post(post_id: int):
     return {"message": "not implemented"}
