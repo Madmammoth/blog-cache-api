83c48b10d15aba8c889a192a26afa18221ac205f
Marat <akhmetzyanovmm@gmail.com>
2026-03-11 10:52:49 +0300

    fix Redis client configuration

--- app/cache/redis_config.py
+++ app/cache/redis_config.py
@@ -3,7 +3,10 @@ import redis.asyncio as redis
 from app.core.config import settings
 
 redis_client = redis.Redis(
-    host='redis',
-    port=6379,
-    decode_responses=True
+    host=settings.redis_host,
+    port=settings.redis_port,
+    db=settings.redis_db,
+    username=settings.redis_username,
+    password=settings.redis_password.get_secret_value(),
+    decode_responses=True,
 )
--- app/core/config.py
+++ app/core/config.py
@@ -6,28 +6,33 @@ from sqlalchemy import URL
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_file=".env", env_prefix="POSTGRES_", extra="ignore"
-    )
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
-    user: str
-    password: SecretStr
-    host: str
-    port: int
-    db: str
-    driver: str = "psycopg"
-    schema: str = "postgresql"
+    postgres_user: str
+    postgres_password: SecretStr
+    postgres_host: str
+    postgres_port: int
+    postgres_db: str
+    postgres_driver: str = "psycopg"
+    postgres_schema: str = "postgresql"
+
+    redis_host: str
+    redis_port: int
+    redis_db: int
+    redis_username: str
+    redis_password: SecretStr
 
     @computed_field
     def dsn(self) -> str:
         url_obj = URL.create(
-            drivername=f"{self.schema}+{self.driver}",
-            username=f"{self.user}",
-            password=f"{self.password.get_secret_value()}",
-            host=self.host,
-            port=self.port,
-            database=self.db,
+            drivername=f"{self.postgres_schema}+{self.postgres_driver}",
+            username=self.postgres_user,
+            password=self.postgres_password.get_secret_value(),
+            host=self.postgres_host,
+            port=self.postgres_port,
+            database=self.postgres_db,
         )
+
         return url_obj.render_as_string(hide_password=False)
 
 
