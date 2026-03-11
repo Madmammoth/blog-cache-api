51fe589df2c7dbfcfbcc08686bf2786b606c5e62
Marat <akhmetzyanovmm@gmail.com>
2026-03-11 12:49:57 +0300

    correct REST status codes for endpoints

--- app/api/posts.py
+++ app/api/posts.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, status
 
 from app.schemas.post import PostResponse, PostCreate
 from app.services.post_service import PostService
@@ -9,7 +9,11 @@ router = APIRouter(
 )
 
 
-@router.post("", response_model=PostResponse)
+@router.post(
+    "",
+    response_model=PostResponse,
+    status_code=status.HTTP_201_CREATED,
+)
 async def create_post(data: PostCreate):
     post = await PostService.create_post(data)
     return post
@@ -32,7 +36,11 @@ async def update_post(post_id: int, data: PostCreate):
     return post
 
 
-@router.delete("/{post_id}", response_model=PostResponse)
+@router.delete(
+    "/{post_id}",
+    response_model=PostResponse,
+    status_code=status.HTTP_204_NO_CONTENT,
+)
 async def delete_post(post_id: int):
     deleted = await PostService.delete_post(post_id)
 
